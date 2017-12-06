Fixed quaterinon loss by backproping through normalization

src/quaternion_loss.py
@@ -6,13 +6,13 @@ Created on Mon Nov 20 19:05:43 2017
 """
 import torch
 
-def quaternionLoss(preds, conj_labels):
+def quaternionLoss(preds, labels):
         """
         :param preds: quaternion predictions (batch_size, 1)
-        :param conj_labels: conjugate of label quaternions labels (batch_size, 1)
+        :param labels: label quaternions labels (batch_size, 1)
         :return: Loss. Loss is a variable which may have a backward pass performed.
         """
-        conj_labels = conj_labels.float()
+        labels = labels.float()
         preds = preds.float()
         loss = torch.zeros(1)
 
@@ -20,7 +20,8 @@ def quaternionLoss(preds, conj_labels):
             loss = loss.cuda()
         loss = torch.autograd.Variable(loss)
         
-        qn = torch.norm(preds, p=2, dim=1).detach()
+        qn = torch.norm(preds, p=2, dim=1)#.detach()
+        #print(qn)
         preds = preds.div(qn.unsqueeze(1).expand_as(preds))
-        loss = torch.mean(1 - torch.sum(torch.mul(preds, conj_labels.float()), 1)**2)
+        loss = torch.mean(1 - torch.sum(torch.mul(preds, labels.float()), 1)**2)
         return loss