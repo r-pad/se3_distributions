cb2a5123d3c5d1c3fc6a33135ac06bf0f3181e37
bokorn <brian.e.okorn@gmail.com>
2018-01-04 21:50:33 -0500

    added live renderer and broke out rotation processing

---
+++ src/data_preprocessing.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  2 22:24:09 2018
+
+@author: bokorn
+"""
+
+import numpy as np
+import cv2
+
+def calcViewlossVec(size, sigma):
+    band    = np.linspace(-1*size, size, 1 + 2*size, dtype=np.int16)
+    vec     = np.linspace(-1*size, size, 1 + 2*size, dtype=np.float)
+    prob    = np.exp(-1 * abs(vec) / sigma)
+    prob    = prob / np.sum(prob)
+
+    return band, prob
+
+def label2Probs(angle, angle_bins = 360, band_width = 7, sigma=5):
+    '''
+    Returns three arrays for the viewpoint labels, one for each rotation axis.
+    A label is given by angle
+    :return:
+    '''
+    # Calculate object multiplier
+    angle = int(angle)
+    label = np.zeros(angle_bins, dtype=np.float)
+    
+    # calculate probabilities
+    band, prob = calcViewlossVec(band_width, sigma)
+
+    for i in band:
+        ind = np.mod(angle + i + 360, 360)
+        label[ind] = prob[i + band_width]
+
+    return label
+
+def resizeAndPad(img, size, padColor=255.0):
+
+    h, w = img.shape[:2]
+    sh, sw = size
+
+    # interpolation method
+    if h > sh or w > sw: # shrinking image
+        interp = cv2.INTER_AREA
+    else: # stretching image
+        interp = cv2.INTER_CUBIC
+
+    # aspect ratio of image
+    aspect = w/h
+
+    # compute scaling and pad sizing
+    if aspect > 1: # horizontal image
+        new_w = sw
+        new_h = np.round(new_w/aspect).astype(int)
+        pad_vert = (sh-new_h)/2
+        pad_top, pad_bot = np.floor(pad_vert).astype(int), np.ceil(pad_vert).astype(int)
+        pad_left, pad_right = 0, 0
+    elif aspect < 1: # vertical image
+        new_h = sh
+        new_w = np.round(new_h*aspect).astype(int)
+        pad_horz = (sw-new_w)/2
+        pad_left, pad_right = np.floor(pad_horz).astype(int), np.ceil(pad_horz).astype(int)
+        pad_top, pad_bot = 0, 0
+    else: # square image
+        new_h, new_w = sh, sw
+        pad_left, pad_right, pad_top, pad_bot = 0, 0, 0, 0
+
+    # set pad color
+    if len(img.shape) is 3 and not isinstance(padColor, (list, tuple, np.ndarray)): # color image but only one color provided
+        padColor = [padColor]*3
+
+    # scale and pad
+    scaled_img = cv2.resize(img, (new_w, new_h), interpolation=interp)
+    scaled_img = cv2.copyMakeBorder(scaled_img, pad_top, pad_bot, pad_left, pad_right, borderType=cv2.BORDER_CONSTANT, value=padColor)
+
+    return scaled_img
+    
+def transparentOverlay(foreground, background=None, pos=(0,0),scale = 1):
+    """
+    :param foreground: transparent Image (BGRA)
+    :param background: Input Color Background Image
+    :param pos:  position where the image to be blit.
+    :param scale : scale factor of transparent image.
+    :return: Overlayed image
+    """
+    
+    if(scale != 1):
+        foreground = cv2.resize(foreground, None,fx=scale,fy=scale)
+
+    alpha = foreground[:,:,3:].astype(float)/255    
+    
+    if(background is None):
+        background = alpha*foreground[:,:,:3] + 255.0*(1.0-alpha)
+    else:
+        h,w,_ = foreground.shape
+        rows,cols,_ = background.shape
+        
+        y0,x0 = pos[0],pos[1]
+        y1 = min(y0+h, rows)
+        x1 = min(x0+w, cols)
+        background[y0:y1,x0:x1,:] = alpha*foreground[:,:,:3] + (1.0-alpha)*background[y0:y1,x0:x1,:]
+    
+    return background
+
+
+
+def uniformRandomQuaternion():
+    u = np.random.rand(3)
+    return uniform2Quat(u)
+
+def uniform2Quat(u):
+    r1 = np.sqrt(1-u[0])
+    r2 = np.sqrt(u[0])
+    theta1 = 2.0*np.pi*u[1]
+    theta2 = 2.0*np.pi*u[2]
+    return np.array([r1*np.sin(theta1), r1*np.cos(theta1), r2*np.sin(theta2), r2*np.cos(theta2)])
+
+def quat2Uniform(q):
+    u1 = (q[2]**2 + q[3]**2)
+    u2 = np.arctan2(q[0],q[1])/(2.0*np.pi)
+    u3 = np.arctan2(q[2],q[3])/(2.0*np.pi)
+    return np.array([u1, u2, u3])
--- src/pose_renderer.py
+++ src/pose_renderer.py
@@ -5,11 +5,24 @@ Created on Tue Nov 21 17:35:53 2017
 @author: bokorn
 """
 
-import os
 import sys
 import math
 import random
 import numpy as np
+import os, tempfile, glob, shutil
+
+if __name__ != '__main__':
+    import cv2
+
+render_root_folder = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, render_root_folder)
+import transformations as tf
+
+blank_blend_file_path = os.path.join(render_root_folder, 'blank.blend') 
+#Should be a system variable
+blender_executable_path = '/home/bokorn/src/blender/blender-2.79-linux-glibc219-x86_64/blender'
+render_code = os.path.abspath(__file__)
+
 
 def camera2quat(azimuth_deg, elevation_deg, tilt_deg):
     cx, cy, cz = objCentenedCameraPos(1, azimuth_deg, elevation_deg)
@@ -115,60 +128,99 @@ def objCentenedCameraPos(dist, azimuth_deg, elevation_deg):
     y = (dist * math.sin(theta) * math.cos(phi))
     z = (dist * math.sin(phi))
     return (x, y, z)
-    
-class blenderPoseRenderer(object):
-    def __init__(self,
-                 light_num_bounds = [0, 6],
-                 light_dist_bounds = [8, 20]):
-        self.light_num_lower = light_num_bounds[0]
-        self.light_num_upper = light_num_bounds[1]
-        self.light_dist_lower = light_dist_bounds[0]
-        self.light_dist_upper = light_dist_bounds[1]
-        self.light_azimuth_lower = 0
-        self.light_azimuth_upper = 360
-        self.light_elevation_lower = -90
-        self.light_elevation_upper = 90
-        self.light_energy_mean = 2
-        self.light_energy_std = 2
-        self.light_environment_energy_lower = 0
-        self.light_environment_energy_upper = 1
-    
 
+def renderView(model_file, pose_quats, camera_dist, filenames = None):
+    temp_dirname = tempfile.mkdtemp()
+    pose_quats = str([q.tolist() for q in pose_quats]).replace(',','').replace('[','').replace(']','')
+    render_cmd = '{} {} --background --python {} -- --shape_file {} --pos_quats {} --camera_dist {} --image_folder {} > /dev/null 2>&1'.format(
+            blender_executable_path, blank_blend_file_path, render_code, model_file, pose_quats, camera_dist, temp_dirname)
     
-    def renderObject(self, shape_file, pos_quat, 
-                     image_folder = None,
-                     shape_md5 = None, 
-                     shape_synset = None):
-        import bpy
-        # Input parameters
-        if image_folder is not None and not os.path.exists(image_folder):
-            os.mkdir(image_folder)
+    try:
+        os.system(render_cmd)
+        image_filenames = sorted(glob.glob(temp_dirname+'/*.png'))
+        
+        rendered_imgs = []
+        
+        if(filenames is None):
+            for render_filename in image_filenames:
+                img = cv2.imread(render_filename, cv2.IMREAD_UNCHANGED)
+                rendered_imgs.append(img)
+        elif(len(image_filenames) == len(filenames)):
+            for render_filename, save_filename  in zip(image_filenames, filenames):
+                shutil.move(render_filename, save_filename)
+        else:
+            raise AssertionError('filenames must be None or same size as pose_quats, Expected {}, Got {}'.format(len(image_filenames), len(filenames)))
 
-        bpy.ops.import_scene.obj(filepath=shape_file) 
+        shutil.rmtree(temp_dirname)
         
-        bpy.context.scene.render.alpha_mode = 'TRANSPARENT'
-        #bpy.context.scene.render.use_shadows = False
-        #bpy.context.scene.render.use_raytrace = False
+    except Exception as e:
+        shutil.rmtree(temp_dirname)
+        raise(e)
         
-        bpy.data.objects['Lamp'].data.energy = 0
-        
-        #m.subsurface_scattering.use = True
-        
-        camObj = bpy.data.objects['Camera']
-        # camObj.data.lens_unit = 'FOV'
-        # camObj.data.angle = 0.2
-        
-        # set lights
-        bpy.ops.object.select_all(action='TOGGLE')
-        if 'Lamp' in list(bpy.data.objects.keys()):
-            bpy.data.objects['Lamp'].select = True # remove default light
-        bpy.ops.object.delete()
-        
-        azimuth_deg = param[0]
-        elevation_deg = param[1]
-        theta_deg = param[2]
-        rho = param[3]
+    return rendered_imgs
+
+
+def main():
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
     
+    parser.add_argument('--shape_file', type=str, required=True)
+    parser.add_argument('--pos_quats', nargs='+', type=float, required=True)
+    parser.add_argument('--camera_dist', type=float, required=True)
+    parser.add_argument('--image_folder', type=str, default='./test_images')
+    
+    parser.add_argument('--light_num_lower', type=int, default=1)
+    parser.add_argument('--light_num_upper', type=int, default=6)
+    parser.add_argument('--light_dist_lower', type=float, default=8)
+    parser.add_argument('--light_dist_upper', type=float, default=20)
+    parser.add_argument('--light_azimuth_lower', type=float, default=0)
+    parser.add_argument('--light_azimuth_upper', type=float, default=360)
+    parser.add_argument('--light_elevation_lower', type=float, default=-90)
+    parser.add_argument('--light_elevation_upper', type=float, default=90)
+    parser.add_argument('--light_energy_mean', type=float, default=2)
+    parser.add_argument('--light_energy_std', type=float, default=2)
+    parser.add_argument('--light_environment_energy_lower', type=float, default=0)
+    parser.add_argument('--light_environment_energy_upper', type=float, default=1)
+
+    args = parser.parse_args(sys.argv[6:])
+
+    import bpy
+    # Input parameters
+    if args.image_folder is not None and not os.path.exists(args.image_folder):
+        os.mkdir(args.image_folder)
+
+    bpy.ops.import_scene.obj(filepath=args.shape_file) 
+    
+    bpy.context.scene.render.alpha_mode = 'TRANSPARENT'
+    #bpy.context.scene.render.use_shadows = False
+    #bpy.context.scene.render.use_raytrace = False
+    
+    bpy.data.objects['Lamp'].data.energy = 0
+    
+    #m.subsurface_scattering.use = True
+    
+    camObj = bpy.data.objects['Camera']
+    # camObj.data.lens_unit = 'FOV'
+    # camObj.data.angle = 0.2
+        
+    # set lights
+    bpy.ops.object.select_all(action='TOGGLE')
+    if 'Lamp' in list(bpy.data.objects.keys()):
+        bpy.data.objects['Lamp'].select = True # remove default light
+    bpy.ops.object.delete()
+
+    assert len(args.pos_quats)%4==0, 'pos_quats must have multiple of 4 elements, Recieved {}'.format(len(args.pos_quats))
+
+    num_quats = len(args.pos_quats)//4
+    pose_quat_list = [np.array(args.pos_quats[(4*j):(4*j+4)]) for j in range(num_quats)]
+
+    for pose_num, pos_quat in enumerate(pose_quat_list):
+        camera_mat = np.eye(4)
+        camera_mat[0,3] = args.camera_dist
+        view_mat = tf.quaternion_matrix(tf.quaternion_inverse(pos_quat)).dot(camera_mat)
+        cam_pos = view_mat[:3,3]              
+        cam_quat = tf.quaternion_from_matrix(view_mat)
         # clear default lights
         bpy.ops.object.select_by_type(type='LAMP')
         bpy.ops.object.delete(use_global=False)
@@ -176,37 +228,44 @@ class blenderPoseRenderer(object):
         # set environment lighting
         #bpy.context.space_data.context = 'WORLD'
         bpy.context.scene.world.light_settings.use_environment_light = True
-        bpy.context.scene.world.light_settings.environment_energy = np.random.uniform(self.light_environment_energy_lower, self.light_environment_energy_upper)
+        bpy.context.scene.world.light_settings.environment_energy = np.random.uniform(args.light_environment_energy_lower, args.light_environment_energy_upper)
         bpy.context.scene.world.light_settings.environment_color = 'PLAIN'
     
         # set point lights
-        for i in range(random.randint(self.light_num_lower,self.light_num_upper)):
-            light_azimuth_deg = np.random.uniform(self.light_azimuth_lower, self.light_azimuth_upper)
-            light_elevation_deg  = np.random.uniform(self.light_elevation_lower, self.light_elevation_upper)
-            light_dist = np.random.uniform(self.light_dist_lower, self.light_dist_upper)
-            lx, ly, lz = self.objCentenedCameraPos(light_dist, light_azimuth_deg, light_elevation_deg)
+        for i in range(random.randint(args.light_num_lower,args.light_num_upper)):
+            light_azimuth_deg = np.random.uniform(args.light_azimuth_lower, args.light_azimuth_upper)
+            light_elevation_deg  = np.random.uniform(args.light_elevation_lower, args.light_elevation_upper)
+            light_dist = np.random.uniform(args.light_dist_lower, args.light_dist_upper)
+            lx, ly, lz = objCentenedCameraPos(light_dist, light_azimuth_deg, light_elevation_deg)
             bpy.ops.object.lamp_add(type='POINT', view_align = False, location=(lx, ly, lz))
-            bpy.data.objects['Point'].data.energy = np.random.normal(self.light_energy_mean, self.light_energy_std)
+            bpy.data.objects['Point'].data.energy = np.random.normal(args.light_energy_mean, args.light_energy_std)
     
-        cx, cy, cz = objCentenedCameraPos(rho, azimuth_deg, elevation_deg)
-        q1 = camPosToQuaternion(cx, cy, cz)
-        q2 = camRotQuaternion(cx, cy, cz, theta_deg)
-        q = quaternionProduct(q2, q1)
-        camObj.location[0] = cx
-        camObj.location[1] = cy 
-        camObj.location[2] = cz
+        camera_mat = np.eye(4)
+        
+        camera_mat[0,3] = args.camera_dist
+        view_mat = tf.quaternion_matrix(tf.quaternion_inverse(pos_quat)).dot(camera_mat)
+        cam_pos = view_mat[:3,3]              
+        ro_mat_pre = np.array([[1,0,0],[0,0,1],[0,-1,0]])
+        ro_mat_post = np.array([[0,0,1],[0,-1,0],[1,0,0]])
+        cam_rot = ro_mat_pre.dot(view_mat[:3,:3].T.dot(ro_mat_post))
+        cam_mat = np.eye(4)       
+        cam_mat[:3,:3] = cam_rot
+        cam_quat = tf.quaternion_from_matrix(cam_mat)
+        
+        camObj.location[0] = cam_pos[0]
+        camObj.location[1] = cam_pos[1]
+        camObj.location[2] = cam_pos[2]
         camObj.rotation_mode = 'QUATERNION'
-        camObj.rotation_quaternion[0] = q[0]
-        camObj.rotation_quaternion[1] = q[1]
-        camObj.rotation_quaternion[2] = q[2]
-        camObj.rotation_quaternion[3] = q[3]
-        # ** multiply tilt by -1 to match pascal3d annotations **
-        theta_deg = (-1*theta_deg)%360
-        if(image_folder is not None):
-            syn_image_file = './%s_%s_a%03d_e%03d_t%03d_d%03d.png' % (shape_synset, shape_md5, round(azimuth_deg), round(elevation_deg), round(theta_deg), round(rho))
-            bpy.data.scenes['Scene'].render.filepath = os.path.join(image_folder, syn_image_file)
-            bpy.ops.render.render( write_still=True )
-            return None
-        else:
-            bpy.ops.render.render()
-            
+        # Blender is [w,x,y,z]
+        # Transform is [x,y,z,w]
+        camObj.rotation_quaternion[0] = cam_quat[0]
+        camObj.rotation_quaternion[1] = cam_quat[1]
+        camObj.rotation_quaternion[2] = cam_quat[2]
+        camObj.rotation_quaternion[3] = cam_quat[3]
+
+        syn_image_file = '{}.png'.format(pose_num)
+        bpy.data.scenes['Scene'].render.filepath = os.path.join(args.image_folder, syn_image_file)
+        bpy.ops.render.render( write_still=True )
+
+if __name__=='__main__':
+    main()
