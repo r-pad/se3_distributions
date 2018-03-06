86608a308a40b7fe766a0bf63ac24da73e692bc7
Brian Okorn <bokorn@compute-0-7.local>
2018-03-06 05:37:43 +0000

    added quat loop loss

--- src/generic_pose/losses/quaternion_loss.py
+++ src/generic_pose/losses/quaternion_loss.py
@@ -16,6 +16,7 @@ def quaternionLoss(preds, labels):
         :return: Loss. Loss is a variable which may have a backward pass performed.
         """
         labels = labels.float()
+        labels *= torch.sign(labels[:,3]).unsqueeze(dim=1)
         preds = preds.float()
         loss = torch.zeros(1)
 
@@ -37,9 +38,47 @@ def quaternionError(preds, labels):
     for inst_id in range(batch_size):
         est_q = est_quats[inst_id]/np.linalg.norm(est_quats[inst_id])
         est_q *= np.sign(est_q[3])
-        diff = quatAngularDiff(est_q, true_quats[inst_id])
+        true_q = true_quats[inst_id]
+        true_q *= np.sign(true_q[3])
+        diff = quatAngularDiff(est_q, true_q)
         if(diff > np.pi):
             diff = 2.0*np.pi - diff
         acc += diff
 
     return acc/batch_size
+
+def loopConsistencyLoss(loop_transforms, calc_angle=False):
+    n = loop_transforms[0].size(0)
+    q_loop = torch.zeros(n, 4).float()
+    q_loop[:,3] = 1.0
+    
+    loss = torch.zeros(1)
+
+    if torch.cuda.is_available():
+        q_loop = q_loop.cuda()
+        loss = loss.cuda()
+        
+    q_loop = torch.autograd.Variable(q_loop)
+    loss = torch.autograd.Variable(loss)
+ 
+    for q in loop_transforms:
+        qn = torch.norm(q, p=2, dim=1)#.detach()
+        q = q.div(qn.unsqueeze(1).expand_as(q)).float()
+
+        q_loop = quaternionMultiply(q_loop, q)
+        
+    loss = torch.mean(1 - q_loop[:,3]**2)
+    if(calc_angle):
+        diff = 2*np.arccos(q_loop[:,3].data.cpu().numpy())
+        angle = np.mean(np.abs(diff - 2*np.pi*(diff > np.pi)))        
+    else:
+        angle = None
+        
+    return loss, angle
+
+def quaternionMultiply(q1, q2):
+    
+    return torch.stack([ q2[:,0]*q1[:,3] + q2[:,1]*q1[:,2] - q2[:,2]*q1[:,1] + q2[:,3]*q1[:,0],
+                        -q2[:,0]*q1[:,2] + q2[:,1]*q1[:,3] + q2[:,2]*q1[:,0] + q2[:,3]*q1[:,1],
+                         q2[:,0]*q1[:,1] - q2[:,1]*q1[:,0] + q2[:,2]*q1[:,3] + q2[:,3]*q1[:,2],
+                        -q2[:,0]*q1[:,0] - q2[:,1]*q1[:,1] - q2[:,2]*q1[:,2] + q2[:,3]*q1[:,3]], dim=1)
