9d8d5ef22788d82309d630765cddb70d1c8aaba9
Brian Okorn <bokorn@andrew.cmu.edu>
2018-08-24 20:13:30 +0000

    Updated to_var and to_np to work better with pytorch version in 3.6

--- src/generic_pose/utils/__init__.py
+++ src/generic_pose/utils/__init__.py
@@ -7,12 +7,12 @@ from torch.autograd import Variable
 
 def to_np(x):
     if torch.is_tensor(x):
-        return x.cpu().numpy()
+        return x.detach().cpu().numpy()
     else:
         return x.data.cpu().numpy()
 
-def to_var(x):
+def to_var(x, requires_grad = True):
     if torch.cuda.is_available():
         x = x.cuda()
-    return Variable(x)
+    return Variable(x, requires_grad=requires_grad)
 
