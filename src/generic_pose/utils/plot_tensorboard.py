c88ef4657a12d25e2e2917088c53d38048565f07
Brian Okorn <bokorn@andrew.cmu.edu>
2018-10-15 19:29:37 -0400

    Added table ploting

--- src/generic_pose/utils/plot_tensorboard.py
+++ src/generic_pose/utils/plot_tensorboard.py
@@ -18,20 +18,27 @@ def ewma(data, alpha):
     return out
 
 def plotScalar(tag, data, image_prefix, smoothing=0.0, wall_time = None,
-               labels=None, xlabel=None, ylabel=None, title=None):
+               labels=None, xlabel=None, ylabel=None, title=None, 
+               max_step=None):
     plt.gcf().clear()
     fig = plt.figure()
     ax = plt.subplot(111)
-
     print(ylabel)
+    cell_text = []
     for d, lbl in zip(data, labels):
-        p = ax.plot(d[:,0], ewma(d[:,1], 1-smoothing), label=lbl, linewidth=0.5)
-        ax.plot(d[:,0], d[:,1], color = p[-1].get_color(), alpha=0.1)
+        end = np.where(d[:,0] > max_step)[0]
+        if end.size > 0:
+            end = end[0]
+        else:
+            end = None
+        p = ax.plot(d[:end,0], ewma(d[:end,1], 1-smoothing), label=lbl, linewidth=0.5)
+        ax.plot(d[:end,0], d[:end,1], color = p[-1].get_color(), alpha=0.1)
         print(lbl)
         print('Final Raw: {}'.format(d[-1,1]))                                      
-        print('Final Smoothed: {}'.format(ewma(d[:,1], 1-smoothing)[-1]))
+        print('Final Smoothed: {}'.format(ewma(d[:end,1], 1-smoothing)[-1]))
+        cell_text.append(['{:.2f}'.format(d[-1,1]) + ' ({:.2f})'.format(ewma(d[:end,1], 1-smoothing)[-1])])
     print()
-
+    
     if(labels is not None):
         box = ax.get_position()
         ax.set_position([box.x0, box.y0 + box.height * 0.1,
@@ -41,7 +48,7 @@ def plotScalar(tag, data, image_prefix, smoothing=0.0, wall_time = None,
                   fancybox=True, shadow=True, ncol=5)
     if(wall_time is not None):
         ax_wt = ax.twiny()
-        wall_time = np.array(wall_time)
+        wall_time = np.array(wall_time)[:end]
         wt_hrs = (wall_time - wall_time[0])*np.array([1,1/3600])
         ax_wt.set_position([box.x0, box.y0 + box.height * 0.1,
                             box.width, box.height * 0.9])
@@ -53,6 +60,21 @@ def plotScalar(tag, data, image_prefix, smoothing=0.0, wall_time = None,
     ax.set_ylabel(ylabel)
     plt.title(title)
     fig.savefig(image_prefix+tag+'_plot.png')
+    
+    plt.gcf().clear()
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    #fig.patch.set_visible(False)
+    ax.axis('off')
+    #ax.axis('tight')
+    plt.table(cellText=cell_text,
+              rowLabels=labels,
+              colLabels=[ylabel],
+              loc='center',
+              )
+    #fig.tight_layout()
+    plt.title(title)
+    fig.savefig(image_prefix+tag+'_table.png')
 
 def main():
     import os
@@ -66,6 +88,7 @@ def main():
     parser.add_argument('--xlabels', nargs='+', default=['Steps'])
     parser.add_argument('--ylabels', nargs='+', default=None)
     parser.add_argument('--titles', nargs='+', default=[''])
+    parser.add_argument('--max_step', type=int, default=None)
     parser.add_argument('--wall_time', dest='wall_time', action='store_true')
     parser.add_argument('--image_prefix', type=str, default='')
     parser.add_argument('--smoothing', type=float, default=0.9)
@@ -91,7 +114,8 @@ def main():
                                           cycle(args.ylabels), cycle(args.titles)):
         plotScalar(tag, data[tag], args.image_prefix, smoothing=args.smoothing, 
                    wall_time=wall_time, labels=args.log_labels, 
-                   xlabel=xlabel, ylabel=ylabel, title=title) 
+                   xlabel=xlabel, ylabel=ylabel, title=title,
+                   max_step=args.max_step) 
 
 if __name__=='__main__':
     main()
